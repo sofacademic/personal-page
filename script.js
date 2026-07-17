b6150113bc8cd4beea343ca60d45eedbfb4a68d1
sofacademic <sofjmartino@gmail.com>
2026-07-17 00:45:18 -0400

    javascript added

---
+++ script.js
@@ -0,0 +1,12 @@
+// define elements (the button and img) as variables
+let danceButton = document.getElementById("dance-button");
+let gif = document.getElementById("gif");
+
+// function that changes gif
+function dance() {
+    // changes img's source gif
+    gif.setAttribute("src", "images/kittydance.gif");
+}
+
+// if the button is clicked the function fires
+danceButton.addEventListener("click", dance);
